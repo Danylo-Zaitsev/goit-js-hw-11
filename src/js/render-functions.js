d0ca56c27aa3531c3aa4653aa5aee8e160741822
Danylo Zaitsev <danilverehko@gmail.com>
2025-03-28 16:14:37 +0100

    1

---
+++ src/js/render-functions.js
@@ -0,0 +1,28 @@
+const gallery = document.querySelector('.gallery');
+
+export function createGalleryMarkup(images) {
+  gallery.innerHTML = images
+    .map(
+      image => `
+  <li class="gallery-item">
+    <a class="gallery-link" href="${image.largeImageURL}">
+      <img
+        class="gallery-image"
+        src="${image.webformatURL}"
+        data-source="${image.largeImageURL}"
+        alt="${image.tags}"
+      />
+    </a>
+    <p>Likes: ${image.likes}</p>
+    <p>Views: ${image.views}</p>
+    <p>Comments: ${image.comments}</p>
+    <p>Downloads:  ${image.downloads}</p>
+  </li>`
+    )
+    .join('');
+}
+
+export function clearGallery() {
+  const gallery = document.querySelector('.gallery');
+  gallery.innerHTML = '';
+}
